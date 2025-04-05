d2bf23938362dd773176acb5707986deaa669733
SungStack <s9292909@gmail.com>
2025-04-05 22:58:52 +0900

    Add : Alert 컴포넌트 라이브러리

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,3 +2,4 @@ export { BasicButton } from "./button";
 export { Loading } from "./loading";
 export { SnackbarNotification } from "./notification";
 export { BasicModal } from "./modal";
+export { Alert } from "./alert"

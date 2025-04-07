31767d1b562c484006c82d26e3e73359ccb53357
YeonCheols <s9292909@gmail.com>
2025-04-07 14:53:03 +0900

    Fix: package name

--- apps/docs/.eslintrc.cjs
+++ apps/docs/.eslintrc.cjs
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/storybook.js"],
+  extends: ["@yeoncheol/eslint-config/storybook.js"],
 };
--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  extends: ["@yeoncheol/eslint-config/react.js"],
 };
